4a449d9807ce9ed70b1d6ba85ec809a0ab85f60b
Rhealai <44374177+Rhealai@users.noreply.github.com>
2019-09-04 02:50:39 +0800

    INI

---
+++ WM_SendMessage/cmdline_process/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.comPort_ComboBox = New System.Windows.Forms.ComboBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.connect_BTN = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer_LBL = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(212, 23)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(299, 301)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(30, 23)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(135, 41)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "WM_SETTEXT"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(30, 92)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(135, 44)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "FindWindowsHwnd"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(30, 153)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(135, 41)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "WM_CLOSE"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(30, 215)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(135, 40)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Cmd -> TextBox"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'comPort_ComboBox
+        '
+        Me.comPort_ComboBox.FormattingEnabled = True
+        Me.comPort_ComboBox.Location = New System.Drawing.Point(30, 296)
+        Me.comPort_ComboBox.Name = "comPort_ComboBox"
+        Me.comPort_ComboBox.Size = New System.Drawing.Size(114, 20)
+        Me.comPort_ComboBox.TabIndex = 5
+        '
+        'Timer1
+        '
+        '
+        'connect_BTN
+        '
+        Me.connect_BTN.Location = New System.Drawing.Point(66, 322)
+        Me.connect_BTN.Name = "connect_BTN"
+        Me.connect_BTN.Size = New System.Drawing.Size(99, 25)
+        Me.connect_BTN.TabIndex = 6
+        Me.connect_BTN.Text = "Connect"
+        Me.connect_BTN.UseVisualStyleBackColor = True
+        '
+        'Timer_LBL
+        '
+        Me.Timer_LBL.AutoSize = True
+        Me.Timer_LBL.Location = New System.Drawing.Point(28, 272)
+        Me.Timer_LBL.Name = "Timer_LBL"
+        Me.Timer_LBL.Size = New System.Drawing.Size(63, 12)
+        Me.Timer_LBL.TabIndex = 7
+        Me.Timer_LBL.Text = "Timer:Status"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(212, 339)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 8
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(307, 339)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 9
+        Me.Button6.Text = "Button6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(550, 374)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Timer_LBL)
+        Me.Controls.Add(Me.connect_BTN)
+        Me.Controls.Add(Me.comPort_ComboBox)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents comPort_ComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents connect_BTN As System.Windows.Forms.Button
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents Timer_LBL As System.Windows.Forms.Label
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+
+End Class
---
+++ WM_SendMessage/cmdline_process/Form1.vb
@@ -0,0 +1,238 @@
+﻿Imports System.IO
+Imports System.Windows.Forms
+Imports System.Text
+Imports System.Runtime.InteropServices
+Imports System
+Imports System.IO.Ports
+
+
+Public Class Form1
+
+    Dim comPORT As String
+    Dim receivedData As String = ""
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim read As String
+        read = Cmd("ver")
+        'TextBox1.Text = read
+
+
+        Dim root As String = "C:\Program Files (x86)\Microchip\MPLABX\v3.55\mplab_ipe"
+        Directory.SetCurrentDirectory(root)
+
+        'TextBox1.Text = cmdline("")
+        cmdline("")
+
+        Timer1.Enabled = False
+        comPORT = ""
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            comPort_ComboBox.Items.Add(sp)
+        Next
+
+
+    End Sub
+
+    Dim cmdprocess As New System.Diagnostics.Process()
+
+    Public Function cmdline(ByVal Command As String) As String
+        cmdprocess.StartInfo.FileName = "cmd.exe"
+        
+        cmdprocess.StartInfo.UseShellExecute = False
+        cmdprocess.StartInfo.RedirectStandardInput = True
+        cmdprocess.StartInfo.RedirectStandardOutput = True
+        cmdprocess.StartInfo.RedirectStandardError = True
+        cmdprocess.StartInfo.CreateNoWindow = True
+
+
+        cmdprocess.Start()
+
+        'Process.Start("pk3cmd", "-P16F877A -FC:\Users\Riolai\Desktop\PIC16F877A.hex -M -B")
+        Process.Start("pk3cmd", "-P16F877A -FD:\USER\rioyclai\Desktop\P\PIC16F877A.hex -M -B")
+
+        'Dim root As String = "C:\Program Files\Microchip\MPLABX\v3.00\mplab_ipe"
+        'Directory.SetCurrentDirectory(root)
+
+        'cmdprocess.StandardInput.WriteLine("path=C:\Program Files\Microchip\MPLABX\v3.00\mplab_ipe")
+        'cmdprocess.StandardInput.WriteLine("pk3cmd -P16F877A -FC:\Users\Riolai\Desktop\PIC16F877A.hex -M") '執行指定的命令
+        cmdprocess.StandardInput.WriteLine("exit")  '關閉視窗 
+
+        Dim Result As String = cmdprocess.StandardOutput.ReadToEnd()
+        Return Result
+
+    End Function
+
+    Public Function Cmd(ByVal Command As String) As String
+        Dim process As New System.Diagnostics.Process()
+        process.StartInfo.FileName = "cmd.exe"
+        process.StartInfo.UseShellExecute = False
+        process.StartInfo.RedirectStandardInput = True
+        process.StartInfo.RedirectStandardOutput = True
+        process.StartInfo.RedirectStandardError = True
+        process.StartInfo.CreateNoWindow = True
+        process.Start()
+        process.StandardInput.WriteLine(Command) '執行指定的命令 
+
+        process.StandardInput.WriteLine("exit")  '關閉視窗 
+
+        Dim Result As String = process.StandardOutput.ReadToEnd()
+        process.Close()
+        Dim L As New List(Of String)
+        L.AddRange(Result.Split(Chr(13)))
+        L.RemoveAt(0) '移除首行 
+        L.RemoveAt(0) '移除首行     
+        L.RemoveAt(0) '移除首行 
+        L.RemoveAt(0) '移除首行 
+        L.RemoveAt(0) '移除首行 
+        L.RemoveAt(L.Count - 1) '移除末行 
+        L.RemoveAt(L.Count - 1) '移除末行 
+        L.RemoveAt(L.Count - 1) '移除末行 
+        Dim SB As New System.Text.StringBuilder
+        For Each S In L
+            SB.AppendLine(S)
+        Next
+        Return SB.ToString
+    End Function
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox1.Text = ""
+        TextBox1.Text = Cmd("ipconfig")
+    End Sub
+
+    Declare Auto Function FindWindow Lib "user32.dll" _
+    (ByVal IpClassName As String, ByVal IpWindowName As String) As Integer
+
+    Declare Auto Function SendMessage Lib "user32.dll" _
+        (ByVal hwnd As Integer, ByVal wMsg As Integer,
+         ByVal wParam As Integer, ByVal lParam As String) As Integer
+
+    Declare Auto Function PostMessage Lib "user32.dll" _
+    (ByVal hwnd As Integer, ByVal wMsg As Integer,
+     ByVal wParam As Integer, ByVal lParam As String) As Boolean
+
+    Declare Auto Function ShowWindow Lib "user32.dll" _
+        (ByVal hwnd As Integer, ByVal nCmdShow As Integer) As Integer
+
+    Public Const WM_SETTEXT = &HC
+    Public Const WM_COPYDATA As Integer = &H4A
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim hWnd As Integer
+        hWnd = FindWindow(vbNullString, "cmd.exe")
+
+        TextBox1.Text = hWnd.ToString
+
+        Dim str As String = "XXX"
+        Dim v As StringBuilder
+
+        v = New StringBuilder(str)
+
+        SendMessage(hWnd, WM_SETTEXT, 0, str)
+        PostMessage(hWnd, WM_KEYDOWN, VK_ENTER, 0)
+
+    End Sub
+
+
+    Public Const WM_CHAR = &H66
+    Public Const WM_KEYDOWN = &H100
+    Public Const VK_ENTER As Integer = &HD
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        KeyDown_Enter()
+    End Sub
+
+    Dim path As String = "C:\Program Files (x86)\Microchip\MPLABX\v3.55\mplab_ipe\"
+
+    Sub KeyDown_Enter()
+        Dim hWnd As Integer
+
+        hWnd = FindWindow(vbNullString, path + "pk3cmd.exe")
+        'TextBox1.Text = hWnd.ToString
+
+        'PostMessage KeyDown 'ENTER'
+        PostMessage(hWnd, WM_KEYDOWN, 13, 0)
+    End Sub
+
+    Const WM_CLOSE = &H10
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        Dim hWnd As Integer
+
+        'hWnd = FindWindow(vbNullString, "C:\Windows\system32\cmd.exe")
+        hWnd = FindWindow(vbNullString, path + "pk3cmd.exe")
+        TextBox1.Text = hWnd.ToString
+
+        PostMessage(hWnd, WM_CLOSE, 0, 0)
+    End Sub
+
+    Private Sub comPort_ComboBox_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles comPort_ComboBox.SelectedIndexChanged
+        If (comPort_ComboBox.SelectedItem <> "") Then
+            comPORT = comPort_ComboBox.SelectedItem
+        End If
+    End Sub
+
+
+    Private Sub connect_BTN_Click(sender As System.Object, e As System.EventArgs) Handles connect_BTN.Click
+        If (connect_BTN.Text = "Connect") Then
+            If (comPORT <> "") Then
+                SerialPort1.Close()
+                SerialPort1.PortName = comPORT
+                SerialPort1.BaudRate = 9600
+                SerialPort1.DataBits = 8
+                SerialPort1.Parity = Parity.None
+                SerialPort1.StopBits = StopBits.One
+                SerialPort1.Handshake = Handshake.None
+                SerialPort1.Encoding = System.Text.Encoding.Default
+                SerialPort1.ReadTimeout = 10000
+
+                SerialPort1.Open()
+                connect_BTN.Text = "Dis-connect"
+                Timer1.Enabled = True
+                Timer_LBL.Text = "Timer: ON"
+            Else
+                MsgBox("Select a COM port first")
+            End If
+        Else
+            SerialPort1.Close()
+            connect_BTN.Text = "Connect"
+            Timer1.Enabled = False
+            Timer_LBL.Text = "Timer: OFF"
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        receivedData = ReceiveSerialData()
+        If (receivedData.Trim() = "Button pushed.") Then
+            KeyDown_Enter()
+        End If
+        TextBox1.Text += receivedData
+    End Sub
+
+    Function ReceiveSerialData() As String
+        Dim Incoming As String
+        Try
+            Incoming = SerialPort1.ReadExisting()
+            If Incoming Is Nothing Then
+                Return "nothing" & vbCrLf
+            Else
+                Return Incoming
+            End If
+        Catch ex As TimeoutException
+            Return "Error: Serial Port read timed out."
+        End Try
+
+    End Function
+
+    Const SW_HIDE = 0
+    Const SW_SHOW = 5
+
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        Dim hWnd As Integer = FindWindow(vbNullString, path + "pk3cmd.exe")
+        ShowWindow(hWnd, SW_HIDE)
+    End Sub
+
+    Private Sub Button6_Click(sender As System.Object, e As System.EventArgs) Handles Button6.Click
+        Dim hWnd As Integer = FindWindow(vbNullString, path + "pk3cmd.exe")
+        ShowWindow(hWnd, SW_SHOW)
+    End Sub
+End Class
