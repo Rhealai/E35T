c2a01662a8ca8dabf41b0eeca944321afd08c43f
Rhealai <44374177+Rhealai@users.noreply.github.com>
2019-09-06 02:28:52 +0800

    Thr

--- WM_SendMessage/cmdline_process/Form1.Designer.vb
+++ WM_SendMessage/cmdline_process/Form1.Designer.vb
@@ -36,6 +36,19 @@ Partial Class Form1
         Me.Button5 = New System.Windows.Forms.Button()
         Me.Button6 = New System.Windows.Forms.Button()
         Me.Button7 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button12 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -44,12 +57,12 @@ Partial Class Form1
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.TextBox1.Size = New System.Drawing.Size(207, 301)
+        Me.TextBox1.Size = New System.Drawing.Size(323, 263)
         Me.TextBox1.TabIndex = 0
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(30, 23)
+        Me.Button2.Location = New System.Drawing.Point(32, 73)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(135, 41)
         Me.Button2.TabIndex = 2
@@ -58,7 +71,7 @@ Partial Class Form1
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(30, 92)
+        Me.Button3.Location = New System.Drawing.Point(32, 23)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(135, 44)
         Me.Button3.TabIndex = 3
@@ -67,7 +80,7 @@ Partial Class Form1
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(30, 153)
+        Me.Button4.Location = New System.Drawing.Point(31, 120)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(135, 41)
         Me.Button4.TabIndex = 4
@@ -76,7 +89,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(30, 215)
+        Me.Button1.Location = New System.Drawing.Point(30, 167)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(135, 40)
         Me.Button1.TabIndex = 1
@@ -86,7 +99,7 @@ Partial Class Form1
         'comPort_ComboBox
         '
         Me.comPort_ComboBox.FormattingEnabled = True
-        Me.comPort_ComboBox.Location = New System.Drawing.Point(30, 296)
+        Me.comPort_ComboBox.Location = New System.Drawing.Point(190, 235)
         Me.comPort_ComboBox.Name = "comPort_ComboBox"
         Me.comPort_ComboBox.Size = New System.Drawing.Size(114, 20)
         Me.comPort_ComboBox.TabIndex = 5
@@ -96,7 +109,7 @@ Partial Class Form1
         '
         'connect_BTN
         '
-        Me.connect_BTN.Location = New System.Drawing.Point(66, 322)
+        Me.connect_BTN.Location = New System.Drawing.Point(226, 261)
         Me.connect_BTN.Name = "connect_BTN"
         Me.connect_BTN.Size = New System.Drawing.Size(99, 25)
         Me.connect_BTN.TabIndex = 6
@@ -106,7 +119,7 @@ Partial Class Form1
         'Timer_LBL
         '
         Me.Timer_LBL.AutoSize = True
-        Me.Timer_LBL.Location = New System.Drawing.Point(28, 272)
+        Me.Timer_LBL.Location = New System.Drawing.Point(188, 211)
         Me.Timer_LBL.Name = "Timer_LBL"
         Me.Timer_LBL.Size = New System.Drawing.Size(63, 12)
         Me.Timer_LBL.TabIndex = 7
@@ -114,36 +127,165 @@ Partial Class Form1
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(212, 339)
+        Me.Button5.Location = New System.Drawing.Point(188, 23)
         Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.Size = New System.Drawing.Size(137, 44)
         Me.Button5.TabIndex = 8
         Me.Button5.Text = "SW_HIDE"
         Me.Button5.UseVisualStyleBackColor = True
         '
         'Button6
         '
-        Me.Button6.Location = New System.Drawing.Point(307, 339)
+        Me.Button6.Location = New System.Drawing.Point(188, 73)
         Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.Size = New System.Drawing.Size(137, 41)
         Me.Button6.TabIndex = 9
         Me.Button6.Text = "SW_SHOW"
         Me.Button6.UseVisualStyleBackColor = True
         '
         'Button7
         '
-        Me.Button7.Location = New System.Drawing.Point(188, 23)
+        Me.Button7.Location = New System.Drawing.Point(188, 167)
         Me.Button7.Name = "Button7"
         Me.Button7.Size = New System.Drawing.Size(137, 41)
         Me.Button7.TabIndex = 10
         Me.Button7.Text = "SendMessage"
         Me.Button7.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("新細明體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.Location = New System.Drawing.Point(28, 300)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 16)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "程式目錄"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(36, 329)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(518, 22)
+        Me.TextBox2.TabIndex = 12
+        Me.TextBox2.Text = "C:\Program Files (x86)\Microchip\MPLABX\v3.55\mplab_ipe"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("新細明體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label2.Location = New System.Drawing.Point(28, 356)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 16)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "指定指令"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(36, 385)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(96, 22)
+        Me.TextBox3.TabIndex = 12
+        Me.TextBox3.Text = "pk3cmd.exe"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("新細明體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label3.Location = New System.Drawing.Point(28, 411)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(104, 16)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "控制視窗名稱"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(36, 440)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(518, 22)
+        Me.TextBox4.TabIndex = 12
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(585, 329)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(69, 22)
+        Me.Button8.TabIndex = 13
+        Me.Button8.Text = "Set"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(585, 385)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(69, 22)
+        Me.Button9.TabIndex = 13
+        Me.Button9.Text = "Set"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(585, 440)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(69, 22)
+        Me.Button10.TabIndex = 13
+        Me.Button10.Text = "Set"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(32, 254)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(134, 38)
+        Me.Button11.TabIndex = 14
+        Me.Button11.Text = "Application Start"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(148, 385)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(405, 22)
+        Me.TextBox5.TabIndex = 15
+        Me.TextBox5.Text = "-P16F877A -FD:\USER\rioyclai\Desktop\P\PIC16F877A.hex -M -B"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("新細明體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label4.Location = New System.Drawing.Point(145, 356)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 16)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "擴充指令"
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(32, 212)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(134, 32)
+        Me.Button12.TabIndex = 16
+        Me.Button12.Text = "Send Message ""Enter"""
+        Me.Button12.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(550, 374)
+        Me.ClientSize = New System.Drawing.Size(710, 474)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button7)
         Me.Controls.Add(Me.Button6)
         Me.Controls.Add(Me.Button5)
@@ -174,5 +316,18 @@ Partial Class Form1
     Friend WithEvents Button5 As System.Windows.Forms.Button
     Friend WithEvents Button6 As System.Windows.Forms.Button
     Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button12 As System.Windows.Forms.Button
 
 End Class
--- WM_SendMessage/cmdline_process/Form1.vb
+++ WM_SendMessage/cmdline_process/Form1.vb
@@ -6,8 +6,6 @@ Imports System
 Imports System.IO.Ports
 Imports System.Threading
 
-
-
 Public Class Form1
 
     Dim comPORT As String
@@ -15,15 +13,19 @@ Public Class Form1
 
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Dim read As String
-        read = Cmd("ver")
+        'Dim read As String
+        'read = Cmd("ver")
         'TextBox1.Text = read
 
-        Dim root As String = "C:\Program Files (x86)\Microchip\MPLABX\v3.55\mplab_ipe"
-        Directory.SetCurrentDirectory(root)
+        'Dim root As String = "C:\Program Files (x86)\Microchip\MPLABX\v3.55\mplab_ipe"
+        'Directory.SetCurrentDirectory(root)
 
-        'TextBox1.Text = cmdline("")
-        'cmdline("")
+        ''TextBox1.Text = cmdline("")
+        ''cmdline("")
+        TextBox2.Text = My.Settings.AppRootPath
+        TextBox3.Text = My.Settings.AppName
+        TextBox5.Text = My.Settings.AppAugmented
+        TextBox4.Text = My.Settings.ControlWinName
 
         Timer1.Enabled = False
         comPORT = ""
@@ -36,32 +38,23 @@ Public Class Form1
 
     Dim cmdprocess As New System.Diagnostics.Process()
 
-    Public Function cmdline(ByVal Command As String) As String
+    Sub AppExecute(ByVal application As String, ByVal augmented As String)
         cmdprocess.StartInfo.FileName = "cmd.exe"
-        
+
         cmdprocess.StartInfo.UseShellExecute = False
         cmdprocess.StartInfo.RedirectStandardInput = True
         cmdprocess.StartInfo.RedirectStandardOutput = True
         cmdprocess.StartInfo.RedirectStandardError = True
         cmdprocess.StartInfo.CreateNoWindow = True
 
-
         cmdprocess.Start()
 
-        'Process.Start("pk3cmd", "-P16F877A -FC:\Users\Riolai\Desktop\PIC16F877A.hex -M -B")
-        Process.Start("pk3cmd", "-P16F877A -FD:\USER\rioyclai\Desktop\P\PIC16F877A.hex -M -B")
+        'Process.Start("pk3cmd", "-P16F877A -FD:\USER\rioyclai\Desktop\P\PIC16F877A.hex -M -B")
+        Process.Start(application, augmented)
 
-        'Dim root As String = "C:\Program Files\Microchip\MPLABX\v3.00\mplab_ipe"
-        'Directory.SetCurrentDirectory(root)
-
-        'cmdprocess.StandardInput.WriteLine("path=C:\Program Files\Microchip\MPLABX\v3.00\mplab_ipe")
-        'cmdprocess.StandardInput.WriteLine("pk3cmd -P16F877A -FC:\Users\Riolai\Desktop\PIC16F877A.hex -M") '執行指定的命令
         cmdprocess.StandardInput.WriteLine("exit")  '關閉視窗 
 
-        Dim Result As String = cmdprocess.StandardOutput.ReadToEnd()
-        Return Result
-
-    End Function
+    End Sub
 
     Public Function Cmd(ByVal Command As String) As String
         Dim process As New System.Diagnostics.Process()
@@ -100,71 +93,6 @@ Public Class Form1
         TextBox1.Text = Cmd("ipconfig")
     End Sub
 
-    Declare Auto Function FindWindow Lib "user32.dll" _
-    (ByVal IpClassName As String, ByVal IpWindowName As String) As Integer
-
-    Declare Auto Function SendMessage Lib "user32.dll" _
-        (ByVal hwnd As Integer, ByVal wMsg As Integer,
-         ByVal wParam As Integer, ByVal lParam As String) As Integer
-
-    Declare Auto Function PostMessage Lib "user32.dll" _
-    (ByVal hwnd As Integer, ByVal wMsg As Integer,
-     ByVal wParam As Integer, ByVal lParam As String) As Boolean
-
-    Declare Auto Function ShowWindow Lib "user32.dll" _
-        (ByVal hwnd As Integer, ByVal nCmdShow As Integer) As Integer
-
-    Public Const WM_SETTEXT = &HC
-    Public Const WM_COPYDATA As Integer = &H4A
-
-
-    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
-        Dim hWnd As Integer
-        hWnd = FindWindow(vbNullString, "cmd.exe")
-
-        TextBox1.Text = hWnd.ToString
-
-        Dim str As String = "XXX"
-        Dim v As StringBuilder
-
-        v = New StringBuilder(str)
-
-        SendMessage(hWnd, WM_SETTEXT, 0, str)
-        PostMessage(hWnd, WM_KEYDOWN, VK_ENTER, 0)
-
-    End Sub
-
-
-    Public Const WM_CHAR = &H66
-    Public Const WM_KEYDOWN = &H100
-    Public Const VK_ENTER As Integer = &HD
-
-    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
-        KeyDown_Enter()
-    End Sub
-
-    Dim path As String = "C:\Program Files (x86)\Microchip\MPLABX\v3.55\mplab_ipe\"
-
-    Sub KeyDown_Enter()
-        Dim hWnd As Integer
-
-        hWnd = FindWindow(vbNullString, path + "pk3cmd.exe")
-        'TextBox1.Text = hWnd.ToString
-
-        'PostMessage KeyDown 'ENTER'
-        PostMessage(hWnd, WM_KEYDOWN, 13, 0)
-    End Sub
-
-    Const WM_CLOSE = &H10
-    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
-        Dim hWnd As Integer
-
-        'hWnd = FindWindow(vbNullString, "C:\Windows\system32\cmd.exe")
-        hWnd = FindWindow(vbNullString, path + "pk3cmd.exe")
-        TextBox1.Text = hWnd.ToString
-
-        PostMessage(hWnd, WM_CLOSE, 0, 0)
-    End Sub
 
     Private Sub comPort_ComboBox_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles comPort_ComboBox.SelectedIndexChanged
         If (comPort_ComboBox.SelectedItem <> "") Then
@@ -236,20 +164,6 @@ Public Class Form1
 
     End Function
 
-    Const SW_HIDE = 0
-    Const SW_SHOW = 5
-
-    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
-        Dim hWnd As Integer = FindWindow(vbNullString, path + "pk3cmd.exe")
-        ShowWindow(hWnd, SW_HIDE)
-    End Sub
-
-    Private Sub Button6_Click(sender As System.Object, e As System.EventArgs) Handles Button6.Click
-        Dim hWnd As Integer = FindWindow(vbNullString, path + "pk3cmd.exe")
-        ShowWindow(hWnd, SW_SHOW)
-    End Sub
-
-
 
     Private Sub Button7_Click(sender As System.Object, e As System.EventArgs) Handles Button7.Click
         If SerialPort1.IsOpen Then
@@ -316,5 +230,124 @@ Public Class Form1
             SerialPort1.Close()
             SerialPort1.Dispose()
         End If
+    End Sub
+
+
+
+
+    Declare Auto Function FindWindow Lib "user32.dll" _
+    (ByVal IpClassName As String, ByVal IpWindowName As String) As Integer
+
+    Declare Auto Function SendMessage Lib "user32.dll" _
+        (ByVal hwnd As Integer, ByVal wMsg As Integer,
+         ByVal wParam As Integer, ByVal lParam As String) As Integer
+
+    Declare Auto Function PostMessage Lib "user32.dll" _
+    (ByVal hwnd As Integer, ByVal wMsg As Integer,
+     ByVal wParam As Integer, ByVal lParam As String) As Boolean
+
+    Declare Auto Function ShowWindow Lib "user32.dll" _
+        (ByVal hwnd As Integer, ByVal nCmdShow As Integer) As Integer
+
+    Public Const WM_SETTEXT = &HC
+    Public Const WM_COPYDATA As Integer = &H4A
+
+
+    Public hWnd As Integer
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        'hWnd = FindWindow(vbNullString, My.Settings.ControlWinName)
+        'TextBox1.Text = hWnd.ToString
+
+        'Dim str As String = "XXX"
+        'Dim v As StringBuilder
+
+        'v = New StringBuilder(str)
+
+        SendMessage(hWnd, WM_SETTEXT, 0, "XXX")
+        PostMessage(hWnd, WM_KEYDOWN, VK_ENTER, 0)
+
+    End Sub
+
+
+    Public Const WM_CHAR = &H66
+    Public Const WM_KEYDOWN = &H100
+    Public Const VK_ENTER As Integer = &HD
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        'Dim hWnd As Integer
+
+        hWnd = FindWindow(vbNullString, My.Settings.ControlWinName)
+        TextBox1.Text = hWnd.ToString
+
+        'KeyDown_Enter()
+    End Sub
+
+    'Dim path As String = "C:\Program Files (x86)\Microchip\MPLABX\v3.55\mplab_ipe\"
+
+    Sub KeyDown_Enter()
+        'Dim hWnd As Integer
+
+        'hWnd = FindWindow(vbNullString, path + "pk3cmd.exe")
+        'TextBox1.Text = hWnd.ToString
+
+        'PostMessage KeyDown 'ENTER'
+        PostMessage(hWnd, WM_KEYDOWN, 13, 0)
+    End Sub
+
+    Const WM_CLOSE = &H10
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        'Dim hWnd As Integer
+
+        'hWnd = FindWindow(vbNullString, "C:\Windows\system32\cmd.exe")
+        'hWnd = FindWindow(vbNullString, path + "pk3cmd.exe")
+        TextBox1.Text = hWnd.ToString
+
+        PostMessage(hWnd, WM_CLOSE, 0, 0)
+    End Sub
+
+    Const SW_HIDE = 0
+    Const SW_SHOW = 5
+
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        'Dim hWnd As Integer = FindWindow(vbNullString, path + "pk3cmd.exe")
+        ShowWindow(hWnd, SW_HIDE)
+    End Sub
+
+    Private Sub Button6_Click(sender As System.Object, e As System.EventArgs) Handles Button6.Click
+        'Dim hWnd As Integer = FindWindow(vbNullString, path + "pk3cmd.exe")
+        ShowWindow(hWnd, SW_SHOW)
+    End Sub
+
+
+    Private Sub Button11_Click(sender As System.Object, e As System.EventArgs) Handles Button11.Click
+        'Dim root As String = "C:\Program Files (x86)\Microchip\MPLABX\v3.55\mplab_ipe"
+        Dim root As String = My.Settings.AppRootPath
+        Directory.SetCurrentDirectory(root)
+        'AppExecute("pk3cmd", "-P16F877A -FD:\USER\rioyclai\Desktop\P\PIC16F877A.hex -M -B")
+        AppExecute(My.Settings.AppName, My.Settings.AppAugmented)
+
+    End Sub
+
+    Private Sub Button10_Click(sender As System.Object, e As System.EventArgs) Handles Button10.Click
+        My.Settings.ControlWinName = TextBox4.Text
+        My.Settings.Save()
+
+    End Sub
+
+    Private Sub Button8_Click(sender As System.Object, e As System.EventArgs) Handles Button8.Click
+        My.Settings.AppRootPath = TextBox2.Text
+        My.Settings.Save()
+    End Sub
+
+    Private Sub Button9_Click(sender As System.Object, e As System.EventArgs) Handles Button9.Click
+        My.Settings.AppName = TextBox3.Text
+        My.Settings.AppAugmented = TextBox5.Text
+        My.Settings.Save()
+    End Sub
+
+    Private Sub Button12_Click(sender As System.Object, e As System.EventArgs) Handles Button12.Click
+        KeyDown_Enter()
     End Sub
 End Class
